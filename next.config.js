7608201c55b272028bbb761e975e338ac07d5de6
yudekio <145836654+yudekio@users.noreply.github.com>
2024-01-10 23:39:27 +0100

    README.md update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["static.wikia.nocookie.net", "pbs.twimg.com", "img.freepik.com"],
-  },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
