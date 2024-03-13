e49f4ff7922ae819737c7c4d61cbc3eed217d4fc
yudekio <145836654+yudekio@users.noreply.github.com>
2024-03-13 10:30:05 +0100

    Some changes

--- app/dbConfig/dbConfig.ts
+++ app/dbConfig/dbConfig.ts
@@ -4,19 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function connect() {
-  try {
-    mongoose.connect(`sample`);
-    const connection = mongoose.connection;
-
-    connection.on("connected", () => {
-      console.log("MongoDB connected successfully");
-    });
-
-    connection.on("error", (err) => {
-      console.log("MongoDB error" + err);
-      process.exit();
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   mongoose.connect(`sample`);
+  //   const connection = mongoose.connection;
+  //   connection.on("connected", () => {
+  //     console.log("MongoDB connected successfully");
+  //   });
+  //   connection.on("error", (err) => {
+  //     console.log("MongoDB error" + err);
+  //     process.exit();
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 }
